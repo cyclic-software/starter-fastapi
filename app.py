49f782dd3f961d37d799e6d4da4a9b878a80028b
Kam Lasater <kam.lasater@cyclic.sh>
2023-08-11 10:10:24 -0400

    Add favicon and switch file names

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse('favicon.ico')
