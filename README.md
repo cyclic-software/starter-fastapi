ba824739ec9439a7cf48f67992739ae20c5f7b7e
Kam Lasater <kam.lasater@cyclic.sh>
2023-08-11 10:28:14 -0400

    Added CRUDL routes

--- README.md
+++ README.md
@@ -28,6 +28,20 @@ Run: `bin/start`
 - runs a `uvicorn` server
 
 
+## Try the server
+
+Schema docs: [http://localhost:8181/docs](http://localhost:8181/docs)
+
+Get an item:
+`curl -i -XGET http://localhost:8181/item/1`
+
+List items:
+`curl -i -XGET http://localhost:8181/items/`
+
+Post an item:
+`curl -i -XPOST http://localhost:8181/items/ --data '{"item_id":1,"name":"Bob"}' -H 'content-type: application/json'`
+
+
 ## Questions / Help
 
 Join us on Discord: [https://discord.cyclic.sh](https://discord.cyclic.sh)
--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+from pydantic import BaseModel
+
+from typing import Optional
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    item_id: int
+    name: Optional[str] = None
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
     return FileResponse('favicon.ico')
+
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/items/")
+async def list_items():
+    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
