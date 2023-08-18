5ae391dbf0ddd51a6cb0473e9d34da5cd0213834
David Abayomi E <iamdavidabayomi@gmail.com>
2023-08-18 02:31:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import google.auth.transport.requests
+from google.oauth2 import service_account
 
 from pydantic import BaseModel
 
 app = FastAPI()
+
+SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
 
 class Item(BaseModel):
@@ -33,3 +37,16 @@ async def list_items():
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
+@app.get("/get-access-token")
+async def get_access_token():
+    """Retrieve a valid access token that can be used to authorize requests.
+
+    :return: Access token.
+    """
+    credentials = service_account.Credentials.from_service_account_file(
+        'service-account.json', scopes=SCOPES)
+    request = google.auth.transport.requests.Request()
+    credentials.refresh(request)
+    # make it an array to be able to use it in the header
+    return {"status": 200, "credentials": credentials.token}
